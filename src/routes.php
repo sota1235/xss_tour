5e00c3eecce300266f1af79650f52d202d35a395
sota1235 <sota1235@gmail.com>
2016-05-31 22:47:02 +0900

    modify: add level1 page

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,12 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
+$app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/level/{number}', function ($request, $response, $args) {
+    $number = $request->getAttribute('number');
+
+    return $this->renderer->render($response, "level{$number}.phtml", $args);
 });
