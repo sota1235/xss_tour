8978bb70bbc3db0722aefe950b42720718d39965
sota1235 <sota1235@gmail.com>
2016-06-02 04:03:11 +0900

    modify: add e helper

--- src/helpers.php
+++ src/helpers.php
@@ -3,3 +3,16 @@
 /**
  * Helpers
  */
+
+if (! function_exists('e')) {
+    /**
+     * Escape HTML string
+     *
+     * @param string  $str
+     *
+     * @return string
+     */
+    function e($str) {
+        return htmlspecialchars($str, ENT_QUOTES);
+    }
+}
