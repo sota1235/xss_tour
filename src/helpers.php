96a86f38c2e254f13d7d70e4f147fad357df283d
sota1235 <sota1235@gmail.com>
2016-06-02 04:17:46 +0900

    modify: add helper to get param from array

--- src/helpers.php
+++ src/helpers.php
@@ -29,3 +29,26 @@ if (! function_exists('getUnsafeResponse')) {
         return $response->withHeader('x-xss-protection', '0');
     }
 }
+
+if (! function_exists('array_get')) {
+    /**
+     * Get an item from an array.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     *
+     * @return mixed
+     */
+    function array_get($array, $key, $default = null) {
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        return $default;
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -11,33 +11,24 @@ $app->get('/', function ($request, $response, $args) {
 
 // Level1 普通のReflection XSS
 $app->get('/level/1', function ($request, $response, $args) {
-    $params = $request->getQueryParams();
-
-    if (isset($params['name'])) {
-        $args['getName'] = $params['name'];
-    }
+    $params          = $request->getQueryParams();
+    $args['getName'] = array_get($params, 'name', 'no name');
 
     return $this->renderer->render(getUnsafeResponse($response), 'level1.phtml', $args);
 });
 
 // Level2 'script'文字列をサニタイズ
 $app->get('/level/2', function ($request, $response, $args) {
-    $params = $request->getQueryParams();
-
-    if (isset($params['name'])) {
-        $args['getName'] = preg_replace('/script/', '', $params['name']);
-    }
+    $params          = $request->getQueryParams();
+    $args['getName'] = preg_replace('/script/', '', array_get($params, 'name', 'no name'));
 
     return $this->renderer->render(getUnsafeResponse($response), 'level2.phtml', $args);
 });
 
 // Level3 '>', '<'文字列をエスケープ
 $app->get('/level/3', function ($request, $response, $args) {
-    $params = $request->getQueryParams();
-
-    if (isset($params['name'])) {
-        $args['getName'] = $params['name'];
-    }
+    $params          = $request->getQueryParams();
+    $args['getName'] = array_get($params, 'name', 'no name');
 
     return $this->renderer->render(getUnsafeResponse($response), 'level3.phtml', $args);
 });
