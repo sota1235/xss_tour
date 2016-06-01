0606f383504ecf2a4880c765197cc5c5facdb14c
sota1235 <sota1235@gmail.com>
2016-06-02 03:41:48 +0900

    modify: add level1

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,24 @@
 <?php
 // Routes
 
+/**
+ * @deprecated このコードには脆弱性が含まれているので絶対に使用しないでください
+ */
+
 $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/level/{number}', function ($request, $response, $args) {
-    $number = $request->getAttribute('number');
+// 普通のReflection XSS
+$app->get('/level/1', function ($request, $response, $args) {
+    $params = $request->getQueryParams();
 
-    return $this->renderer->render($response, "level{$number}.phtml", $args);
+    // level1ではgetパラメータを付与
+    if (isset($params['name'])) {
+        $args['getName'] = $params['name'];
+    }
+
+    $unsafeResponse = $response->withHeader('x-xss-protection', '0');
+
+    return $this->renderer->render($unsafeResponse, 'level1.phtml', $args);
 });
