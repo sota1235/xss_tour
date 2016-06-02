537f00f33d08bc51c088e46fc5116e6a3e4c0493
sota1235 <sota1235@gmail.com>
2016-06-02 17:15:58 +0900

    add: template for success

--- public/static/all.js
+++ public/static/all.js
@@ -2,4 +2,5 @@ var originAlert = window.alert;
 
 window.alert = function (string) {
   originAlert(string);
+  document.getElementById('success-space').style.display = 'block';
 };
---
+++ templates/_success.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "
+<p id=\"success-space\" class=\"bg-success\" style=\"margin-top: 15px; padding: 15px; display: none;\">Congraturation!! You got 100 pt.</p>
+";
